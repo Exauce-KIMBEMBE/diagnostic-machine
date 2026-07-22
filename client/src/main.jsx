2dc6bf0dff108502f5eb5936d9810681cbf431ae
OpenProgramming <124380852+Exauce-KIMBEMBE@users.noreply.github.com>
2026-07-22 17:06:16 +0000

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")).render(
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error(
+    'Élément HTML avec l’identifiant "root" introuvable.'
+  );
+}
+
+createRoot(rootElement).render(
   <StrictMode>
     <App />
   </StrictMode>
